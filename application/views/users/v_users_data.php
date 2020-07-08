043fbee5b4a8984c5b3374ae0cf2a948933e96fe
Niki <nikidwijananto@gmail.com>
2020-07-09 00:15:44 +0700

    Update v_users_data.php

--- application/views/users/v_users_data.php
+++ application/views/users/v_users_data.php
@@ -1,1 +1,43 @@
-hallo World
+<section class="content-header">
+    <h1>Data Users <small>Users</small></h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="<?= site_url('Dashboard') ?>">
+                <i class="fa fa-dashboard"></i>
+            </a>
+        </li>
+        <li>
+            Users
+        </li>
+    </ol>
+</section>
+
+<section class="content">
+    <div class="box box-primary">
+        <div class="box-header">
+            <h4>Data Users</h4>
+        </div>
+        <div class="box-body table-responsive">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th class="text-center">#</th>
+                        <th class="text-center">Nama</th>
+                        <th class="text-center">Email</th>
+                        <th class="text-center">Alamat</th>
+                        <th class="text-center">Is Active</th>
+                        <th class="text-center">Level</th>
+                        <th class="text-center">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
