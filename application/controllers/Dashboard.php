dd733daeda0e340dc61d722f51362a018372f70d
Niki <nikidwijananto@gmail.com>
2020-07-08 23:56:15 +0700

    Update controllers and views

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,6 @@ class Dashboard extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('v_dashboard');
+        $this->template->load('v_template', 'v_dashboard');
     }
 }
