4f5a8e95fcb661b2e3ea9dee65ffe2b58a0e0653
Niki <nikidwijananto@gmail.com>
2020-07-10 14:55:46 +0700

    Update v_users_edit.php

--- application/views/users/v_users_edit.php
+++ application/views/users/v_users_edit.php
@@ -33,8 +33,31 @@
                 <div class="col-md-6 col-md-offset-3">
                     <form action="">
                         <div class="form-group">
-                            <label for="">Name <i class="text-danger">*</i></label>
+                            <label for="">Nama <i class="text-danger">* </i></label>
                             <input type="text" name="name" id="name" class="form-control" placeholder="Nama Lengkap" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Email <i class="text-danger">* </i></label>
+                            <input type="text" name="email" id="email" placeholder="Email" class="form-control" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Alamat <i class="text-danger">* </i></label>
+                            <textarea name="alamat" id="alamat" placeholder="Alamat" rows="4" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Is Active <i class="text-danger">* </i></label>
+                            <select name="" id="" class="form-control">
+                                <option value="">-- Pilih --</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Level <i class="text-danger">* </i></label>
+                            <select name="" id="" class="form-control">
+                                <option value="">-- Pilih --</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-plus"></i></button>
                         </div>
                     </form>
                 </div>
