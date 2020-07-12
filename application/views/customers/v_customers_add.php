de8a560b99726beffd483bf57ad9b2fb3c97eaa0
Anonymouse-bat <niki.dwijananto@mncgroup.com>
2020-07-12 19:04:37 +0700

    V_customers_add

--- application/views/customers/v_customers_add.php
+++ application/views/customers/v_customers_add.php
@@ -30,7 +30,34 @@
             <div class="row">
                 <div class="col-md-6 col-md-offset-3">
                     <form action="">
-
+                        <div class="form-group">
+                            <label for="">Customers ID <i class="text-danger">*</i></label>
+                            <input type="text" name="customers_id" id="customers_id" class="form-control" placeholder="Customers ID" readonly="true">
+                        </div>
+                        <div class="form-group">
+                            <label for="name_customers">Nama Customers <i class="text-danger">*</i></label>
+                            <input type="text" name="name_customers" id="name_customers" class="form-control" placeholder="Nama Customers">
+                        </div>
+                        <div class="form-group">
+                            <label for="gander_customers">Jenis Kelamin <i class="text-danger">*</i></label>
+                            <select name="gander_customers" id="gander_customers" class="form-control">
+                                <option value="">-- Pilih --</option>
+                                <option value="">Laki-Laki</option>
+                                <option value="">Perempuan</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone_customers">Tlp <i class="text-danger">*</i></label>
+                            <input type="number" name="phone_customers" id="phone_customers" class="form-control" placeholder="No Tlp Customers">
+                        </div>
+                        <div class="form-group">
+                            <label for="address_customers">Alamat <i class="text-danger">*</i></label>
+                            <textarea name="address_customers" id="address_customers" rows="4" class="form-control" placeholder="Alamat Customers"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="reset" class="btn btn-danger"><i class="fa fa-rotate-left"></i></button>
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-plus"></i></button>
+                        </div>
                     </form>
                 </div>
             </div>
