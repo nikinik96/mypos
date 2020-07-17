fb52dd9b05de4c7d59396979ecfc78352ac7e72d
Anonymouse-bat <niki.dwijananto@mncgroup.com>
2020-07-17 19:35:56 +0700

    Update Customers

--- application/models/customers_m.php
+++ application/models/customers_m.php
@@ -5,11 +5,11 @@ class customers_m extends CI_Model
 {
     public function add($post)
     {
-        $params['customers_id'] = $post['customers_id'];
-        $params['name_customers'] = $post['name_customers'];
-        $params['gander_customers'] = $post['gander_customers'];
-        $params['phone_customers'] = $post['phone_customers'];
-        $params['address_customers'] = $post['address_customers'];
+        $params['customers_id']         = $post['customers_id'];
+        $params['name_customers']       = $post['name_customers'];
+        $params['gander_customers']     = $post['gander_customers'];
+        $params['phone_customers']      = $post['phone_customers'];
+        $params['address_customers']    = $post['address_customers'];
 
         $this->db->insert('customers', $params);
     }
