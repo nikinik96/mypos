b9cd0f10526daae04994e5728de9b70dbd38b08c
Niki <nikidwijananto@gmail.com>
2020-07-19 15:32:16 +0700

    Update Unit dan category

--- application/controllers/Items.php
+++ application/controllers/Items.php
@@ -3,6 +3,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Items extends CI_Controller
 {
+
+    function __construct()
+    {
+        parent::__construct();
+        check_not_login();
+    }
+
+
     public function index()
     {
         $this->template->load('v_template', 'products/items/v_items');
--- application/views/products/items/v_items_add.php
+++ application/views/products/items/v_items_add.php
@@ -18,3 +18,54 @@
 
     </ol>
 </section>
+
+<section class="content">
+    <div class="box box-primary">
+        <div class="box-header">
+            <h4>
+                Add Items
+                <div class="pull-right">
+                    <a href="<?= site_url('Items') ?>" class="btn btn-warning">
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </h4>
+        </div>
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="">Barcode <i class="text-danger">*</i></label>
+                            <input type="text" name="" id="" readonly="true" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Product Name <i class="text-danger">*</i></label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Category <i class="text-danger">*</i></label>
+                            <select name="" id="" class="form-control">
+                                <option value="">-- Pilih --</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Unit <i class="text-danger">*</i></label>
+                            <select name="" id="" class="form-control">
+                                <option value="">-- Pilih --</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Price <i class="text-danger">*</i></label>
+                            <input type="text" name="" id="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button type="reset" class="btn btn-danger"><i class="fa fa-rotate-left"></i></button>
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-plus"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
