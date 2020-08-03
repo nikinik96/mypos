f4d2f4cc1a6b7141c4d024bd2df9d10a486508a9
Niki <nikidwijananto@gmail.com>
2020-08-04 01:36:10 +0700

    Add dashboard_m

---
+++ application/models/dashboard_m.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class dashboard_m extends CI_Model
+{
+    public function get_penjualan_hariini()
+    {
+        $this->db->select('');
+    }
+
+    public function get_customers()
+    {
+        $query = $this->db->query("SELECT * FROM customers");
+        $count = $query->num_rows();
+        return $count;
+    }
+
+    public function get_items()
+    {
+        $query = $this->db->query("SELECT * FROM item");
+        $count = $query->num_rows();
+        return $count;
+    }
+    // public function get_suppliers()
+    // {
+    //     $query = $this->db->query("SELECT * FROM suppliers");
+    //     $count = $query->num_rows();
+    //     return $count;
+    // }
+}
