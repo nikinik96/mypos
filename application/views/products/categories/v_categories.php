ab2bb988258243a3ee9063f017b2d12415f56702
Niki <nikidwijananto@gmail.com>
2020-08-06 01:41:48 +0700

    Add New File 060820

---
+++ application/views/products/categories/v_categories.php
@@ -0,0 +1,58 @@
+<section class="content-header">
+    <h1> Categories
+        <small>Kategori Barang</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="<?= site_url('Dashboard') ?>">
+                <i class="fa fa-dashboard"></i>
+            </a>
+        </li>
+        <li>
+            Product
+        </li>
+        <li class="active">Categories</li>
+    </ol>
+</section>
+
+<section class="content">
+    <?= $this->session->flashdata('message'); ?>
+    <div class="box box-primary">
+        <div class="box-header">
+            <h4>Data Categories</h4>
+            <div class="pull-right">
+                <a href="<?php echo site_url('Categories/add') ?>" class="btn btn-primary">
+                    <i class="fa fa-plus"></i>
+                </a>
+            </div>
+        </div>
+        <div class="box-body  table-responsive">
+            <table class="table table-bordered table-striped" id="mytable">
+                <thead>
+                    <tr>
+                        <th class="text-center">#</th>
+                        <th class="text-center">Name</th>
+                        <th class="text-center">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1; ?>
+                    <?php foreach ($row as $key => $data) { ?>
+                        <tr>
+                            <td class="text-center"><?= $no++; ?></td>
+                            <td><?= $data->name_categories ?></td>
+                            <td class="text-center">
+                                <a href="" class="btn btn-success">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                                <a href="<?= site_url('Categories/del/' . $data->categories_id) ?>" class="btn btn-danger" onclick="return confirm('Data <?= $data->name_categories ?> akan dihapus secara permanen, apakah anda yakin  ?');">
+                                    <i class="fa fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
---
+++ application/views/products/categories/v_categories_add.php
@@ -0,0 +1,50 @@
+<section class="content-header">
+    <h1> Categories
+        <small>Kategori Barang</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="<?= site_url('Dashboard') ?>">
+                <i class="fa fa-dashboard"></i>
+            </a>
+        </li>
+        <li>
+            Product
+        </li>
+        <li class="active">Categories</li>
+    </ol>
+</section>
+
+<section class="content">
+    <div class="box box-primary">
+        <div class="box-header">
+            <h4>
+                Add Categories
+                <div class="pull-right">
+                    <a href="<?= site_url('Items') ?>" class="btn btn-warning">
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </h4>
+        </div>
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <form action="<?= site_url('Categories/process') ?>" method="post">
+                        <div class="form-group">
+                            <label> Category Name <i class="text-danger">*</i></label>
+                            <input type="text" name="name_categories" id="name_categories" class="form-control" placeholder="Categories Name" autocomplete="off" autofocus required>
+                        </div>
+                        <button type="reset" class="btn btn-danger">
+                            <i class="fa fa-rotate-left"></i>
+                        </button>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-plus"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
