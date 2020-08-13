f933d02b4b6f77bc441b4c6889733d2524975797
Niki <nikidwijananto@gmail.com>
2020-08-14 00:26:03 +0700

    Update user_login

--- application/views/v_template.php
+++ application/views/v_template.php
@@ -55,15 +55,15 @@
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <img src="<?= base_url('assets') ?>/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                            <span class="hidden-xs">Alexander Pierce</span>
+                            <span class="hidden-xs"><?= ucfirst($this->fungsi->user_login()->level) == 1 ? 'Admin' : 'Member' ?></span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
                             <li class="user-header">
                                 <img src="<?= base_url('assets') ?>/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                                 <p>
-                                    Alexander Pierce - Web Developer
-                                    <small>Member since Nov. 2012</small>
+                                    <?= ucfirst($this->fungsi->user_login()->name) ?>
+                                    <small> <?= ucfirst($this->fungsi->user_login()->alamat) ?> </small>
                                 </p>
                             </li>
                             <li class="user-footer">
@@ -88,7 +88,9 @@
                         <img src="<?= site_url('assets') ?>/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                     </div>
                     <div class="pull-left info">
-                        <p>Alexander Pierce</p>
+                        <p>
+                            <?= ucfirst($this->fungsi->user_login()->name) ?>
+                        </p>
                         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                     </div>
                 </div>
